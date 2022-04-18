26e0dde25b6dea3a79ddf6b4695c4d5c2100befb
Jamil Uddin <jamilbd07@gmail.com>
2022-04-18 12:32:36 +0600

    implement slider block changes in main plugin

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b443b1feefa604eaf2a209dc538b9cfb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ff5ec66d7dea1f1b2d3c515e9c0c04ca');
