e36072b50f0dfa385b0edad198440b17d03b447d
Md. Monir Hossain <fencemonir@gmail.com>
2022-07-04 21:31:28 +0600

    update style handler & controls

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '14ebbb1ca6c3c97a241c52991d467d7a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5018401dda3d32abbb45');
