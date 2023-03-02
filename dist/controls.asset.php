aa87cf3309086fffd20fdc0c3d0382a9709e3b06
Monir <fencemonir@gmail.com>
2023-03-02 17:25:47 +0600

    update controls & font loader

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '540803795dbfcb47fe561bcbd3d499a6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8093a71328a928b9cd74');
